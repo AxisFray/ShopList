1de2b263534aa2ba1a2e572550755d6cf0117c55
AxisFray <113852846+AxisFray@users.noreply.github.com>
2025-10-06 10:06:28 +0200

    Add files via upload

---
+++ ShopList/IsDoneToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace ShopList
+{
+    public class IsDoneToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isDone = (bool)value;
+            return isDone ? Colors.LightGray : Colors.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DoneToTextDecorationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isDone = (bool)value;
+            return isDone ? TextDecorations.Strikethrough : TextDecorations.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
